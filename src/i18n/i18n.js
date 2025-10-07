99670200935e3b0b5bd8281035a875a97e5d1b97
Alexis chapusot <77673271+Nokzs@users.noreply.github.com>
2025-10-07 21:20:02 +0200

    Update i18n.js

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -6,7 +6,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
-
+const baseUrl = import.meta.env.MODE === "production" ? "fst-tchat-front" : ""
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
@@ -25,7 +25,7 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
+      loadPath: `${baseUrl}/locales/{{lng}}/{{ns}}.json`,
     },
   });
 console.log("initialisation");
